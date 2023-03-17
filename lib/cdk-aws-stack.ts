8716bf789a7bf008ca1f7d0380d2ebbdd905255b
House-lovers7 <taisuke.got@gmail.com>
2023-03-18 06:33:39 +0900

    lamda関数のdeploy

--- lib/cdk-aws-stack.ts
+++ lib/cdk-aws-stack.ts
@@ -1,7 +1,16 @@
-import * as cdk from "aws-cdk-lib";
+import * as cdk from 'aws-cdk-lib'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 
 export class CdkAwsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // define an AWS lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler' , {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler',
+    })
+
   }
 }
